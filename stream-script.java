f98b73a168d2dc382bc89a87340c72f960460dee
Vahe Pezeshkian <vaheh.pezeshkian@gmail.com>
2017-12-11 16:34:42 +0400

    Filtering example

--- stream-script.java
+++ stream-script.java
@@ -1,6 +1,6 @@
-Date now = new Date();
-String message = "Hello! This is a message from Java 9!\n" + "Time: " + now.toString();
-System.out.println(message);
+Date now = new Date()
+String message = "Hello! This is a message from Java 9!\n" + "Time: " + now.toString()
+System.out.println(message)
 
 // A class for person info
 class Person {
@@ -19,31 +19,42 @@ class Person {
   public int getAge() {
     return age;
   }
+
+  public String toString() {
+    return name + "\t" + age;
+  }
 }
 
-// A list containing a few Person objects
-List<Person> people = Arrays.asList(
-  new Person("Bob", 22), new Person("Alice", 27), new Person("John", 25), new Person("Vahe", 29),
-  new Person("Zaz", 31), new Person("Mark", 16), new Person("Mike", 19), new Person("Bob", 40)
-);
+// A list of persons
+List<Person> people = new ArrayList<>();
+people.add(new Person("Alice", 20))
+people.add(new Person("Bob", 24))
+people.add(new Person("John", 25))
+people.add(new Person("Mark", 19))
+people.add(new Person("Bob", 40))
+System.out.println("All people")
+people.forEach(System.out::println)
+
+// Remove all persons with age < 21
+List<Person> list = people.stream().map(p -> p.getAge() < 21 ? null : p).filter(p -> p != null).collect(Collectors.toList())
+System.out.println("Removing all with age < 21")
+list.forEach(System.out::println)
 
 // Get sorted names of persons
-Function<List<Person>, String> sortedNames = list -> list.stream().map(Person::getName).sorted().collect(Collectors.joining(","));
+Function<List<Person>, String> sortedNames = list -> list.stream().map(Person::getName).sorted().collect(Collectors.joining(","))
 
 // Get a string presentation of age range
-Function<Integer, String> ageRange = min -> "[" + (min * 10) + "," + ((min + 1) * 10 - 1) + "]";
+Function<Integer, String> ageRange = min -> "[" + (min * 10) + "," + ((min + 1) * 10 - 1) + "]"
 
-Map<Integer, List<Person>> groupByRange = people.stream().collect(Collectors.groupingBy(person -> person.getAge() / 10));
-groupByRange.keySet().forEach(k -> System.out.println(ageRange.apply(k) + " : " + sortedNames.apply(groupByRange.get(k))));
+Map<Integer, List<Person>> groupByRange = people.stream().collect(Collectors.groupingBy(person -> person.getAge() / 10))
+System.out.println("Grouping by age range")
+groupByRange.keySet().forEach(k -> System.out.println(ageRange.apply(k) + " : " + sortedNames.apply(groupByRange.get(k))))
 
-Comparator<Person> byAge    = (p1, p2) -> p1.getAge() - p2.getAge();
-Comparator<Person> byName   = (p1, p2) -> p1.getName().compareTo(p2.getName());
-Consumer<Person> printMax   = p -> { System.out.println(p.getName() + " has maximum age: " + p.getAge()); };
-Consumer<Person> printName  = p -> { System.out.println(p.getName()); };
+Comparator<Person> byAge    = (p1, p2) -> p1.getAge() - p2.getAge()
+Comparator<Person> byName   = (p1, p2) -> p1.getName().compareTo(p2.getName())
+Consumer<Person> printMax   = p -> { System.out.println(p.getName() + " has maximum age: " + p.getAge()); }
+Consumer<Person> printName  = p -> { System.out.println(p.getName()); }
 
-people.stream().max(byAge).ifPresent(printMax);
-
-
-// people.stream().mapToInt(p -> p.getAge()).max().ifPresent(System.out::println);
+people.stream().max(byAge).ifPresent(printMax)
 
 /exit
