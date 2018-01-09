5ef0e97981fd8b667656b3f5af37a51c92b020a7
Vahe Pezeshkian <vaheh.pezeshkian@gmail.com>
2018-01-09 22:25:31 +0400

    Using Comparator.comparing

--- stream-script.java
+++ stream-script.java
@@ -30,14 +30,16 @@ List<Person> people = new ArrayList<>();
 people.add(new Person("Alice", 20))
 people.add(new Person("Bob", 24))
 people.add(new Person("John", 25))
+people.add(new Person("Mike", 19))
 people.add(new Person("Mark", 19))
+people.add(new Person("Anna", 19))
 people.add(new Person("Bob", 40))
-System.out.println("All people")
+System.out.println("\nAll people")
 people.forEach(System.out::println)
 
 // Remove all persons with age < 21
 List<Person> list = people.stream().map(p -> p.getAge() < 21 ? null : p).filter(p -> p != null).collect(Collectors.toList())
-System.out.println("Removing all with age < 21")
+System.out.println("\nRemoving all with age < 21")
 list.forEach(System.out::println)
 
 // Get sorted names of persons
@@ -47,7 +49,7 @@ Function<List<Person>, String> sortedNames = list -> list.stream().map(Person::g
 Function<Integer, String> ageRange = min -> "[" + (min * 10) + "," + ((min + 1) * 10 - 1) + "]"
 
 Map<Integer, List<Person>> groupByRange = people.stream().collect(Collectors.groupingBy(person -> person.getAge() / 10))
-System.out.println("Grouping by age range")
+System.out.println("\nGrouping by age range")
 groupByRange.keySet().forEach(k -> System.out.println(ageRange.apply(k) + " : " + sortedNames.apply(groupByRange.get(k))))
 
 Comparator<Person> byAge    = (p1, p2) -> p1.getAge() - p2.getAge()
@@ -57,4 +59,12 @@ Consumer<Person> printName  = p -> { System.out.println(p.getName()); }
 
 people.stream().max(byAge).ifPresent(printMax)
 
+System.out.println("\nSort using Comparator.comparing(...)")
+Comparator<Person> byAndAndName = Comparator.comparing(Person::getAge).thenComparing(Person::getName)
+people.sort(byAndAndName)
+people.forEach(System.out::println)
+
+System.out.println("\nSort using Comparator.comparing(...).reversed()")
+people.sort(byAndAndName.reversed())
+people.forEach(System.out::println)
 /exit
